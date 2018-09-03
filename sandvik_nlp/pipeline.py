8ed57ca531a0f7028cecd5d6c000f59c39064910
Saket Sinha <saket.sinha89@gmail.com>
2018-09-03 14:22:16 +0530

    Add support for load and save model

--- sandvik_nlp/pipeline.py
+++ sandvik_nlp/pipeline.py
@@ -15,30 +15,64 @@ from sandvik_nlp.beam_utils import NoopCoder
 from sandvik_nlp.beam_utils import CsvFileSource
 from apache_beam.io.filesystem import CompressionTypes
 import pandas as pd
+import sklearn
+from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cross_validation import train_test_split
+import pickle
 
 class Train(beam.DoFn):
     def process(self, element):
         #print(element['sepal_width'])
         df = pd.DataFrame([element], columns=element.keys())
-        print df
+        #print df
+        feature_cols = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+        X = df[feature_cols]
+        target_names = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+        species_map = {k:i for i, k in enumerate(target_names)}
+        df['species'] = df['species'].map(species_map)
+        y = df.species
+
+        clf = KNeighborsClassifier(n_neighbors=1)
+        clf.fit(X, y)
+        with open('filename.pkl', 'wb') as f:
+              pickle.dump(clf, f)
+
+class PredictDoFn(beam.DoFn):
+
+  def process(self, element):
+    with open('filename.pkl', 'rb') as f:
+          clf = pickle.load(f)
+    df = pd.DataFrame([element], columns=element.keys())
+    feature_cols = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+    X = df[feature_cols]
+
+    result = clf.predict(X)
+    return result
 
 def run(options):
 
     template_options = options.view_as(TemplateOptions)
 
 
-    source = beam.io.Read(CsvFileSource('/opt/project/sandvik_nlp/iris/iris.csv'))
+    source_train = beam.io.Read(CsvFileSource('/opt/project/sandvik_nlp/iris/iris_train.csv'))
+    source_test = beam.io.Read(CsvFileSource('/opt/project/sandvik_nlp/iris/iris_test.csv'))
     sink = beam.io.WriteToText(file_path_prefix=template_options.dest, coder=NoopCoder())
 
-    pipeline = beam.Pipeline(options=options)
-    (
-        pipeline
-        | "generate" >> source
-        | 'Train Values' >> beam.ParDo(Train())
-        | "write" >> sink
+    p = beam.Pipeline(options=options)
+    data_process = (
+           p
+           | 'generate' >> source_train
+           | 'train'>> beam.ParDo(Train())
     )
+    predictions = (
+           p
+           | 'test' >> source_test
+           | 'Prediction' >> beam.ParDo(PredictDoFn())
+    )
+    predictions | 'Write' >> sink
 
-    result = pipeline.run()
+    result = p.run()
     success_states = set([PipelineState.DONE])
 
     if template_options.wait:
